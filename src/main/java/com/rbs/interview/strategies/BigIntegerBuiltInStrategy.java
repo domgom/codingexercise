6b63aade9c9ddd8a9b5b98fdd517e9aa02989dac
Domingo Gómez <domgom@gmail.com>
2018-04-23 20:13:18 +0100

    Added first strategy with basic unit testing

---
+++ src/main/java/com/rbs/interview/strategies/BigIntegerBuiltInStrategy.java
@@ -0,0 +1,40 @@
+package com.rbs.interview.strategies;
+
+import com.rbs.interview.PrimesResponse;
+import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.math.BigInteger.TWO;
+
+/**
+ * Uses the internal {@link BigInteger} method {@link BigInteger#isProbablePrime(int)} to iterate among the next
+ * candidates and {@link BigInteger#isProbablePrime(int)} to run the verification.
+ * Delegates most of the implementation for convenience at the expense of further sieve  optimisation.
+ */
+@Component
+public class BigIntegerBuiltInStrategy implements PrimesCalculatorStrategy {
+
+    private static final int CERTAINTY = 100;
+
+    @Override
+    public PrimesResponse calculatePrimesUntilLimit(BigInteger limit) {
+        List<BigInteger> primes = new ArrayList<>();
+
+        BigInteger index = TWO;
+
+        while (index.compareTo(limit) != 1) {
+            if (isPrime(index)) {
+                primes.add(index);
+            }
+            index = index.nextProbablePrime();
+        }
+        return new PrimesResponse(limit, primes);
+    }
+
+    private boolean isPrime(BigInteger candidate) {
+        return candidate.isProbablePrime(CERTAINTY);
+    }
+}
--- src/test/groovy/strategies/SingleThreadStrategySpec.groovy
+++ src/test/groovy/strategies/SingleThreadStrategySpec.groovy
@@ -2,7 +2,7 @@ package strategies
 
 import com.rbs.interview.PrimesApplication
 import com.rbs.interview.PrimesResponse
-import com.rbs.interview.strategies.SingleThreadStrategy
+import com.rbs.interview.strategies.BigIntegerBuiltInStrategy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
@@ -11,13 +11,55 @@ import spock.lang.Specification
 class SingleThreadStrategySpec extends Specification {
 
     @Autowired
-    SingleThreadStrategy singleThreadStrategy
+    BigIntegerBuiltInStrategy singleThreadStrategy
 
     def 'Calculates primes until 6'() {
+        given:
+        BigInteger limit = 6
         when:
-        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(6)
+        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(limit)
         then:
-        response.initial == 6
-        response.primes == [3, 2]
+        response.initial == limit
+        response.primes == [2, 3, 5]
+    }
+
+    def 'Calculates primes until 5 (included)'() {
+        given:
+        BigInteger limit = 5
+        when:
+        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(limit)
+        then:
+        response.initial == limit
+        response.primes == [2, 3, 5]
+    }
+
+    def 'Calculates primes until 1 with no results'() {
+        given:
+        BigInteger limit = 1
+        when:
+        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(limit)
+        then:
+        response.initial == limit
+        response.primes == []
+    }
+
+    def 'Calculates primes until 0 with no results'() {
+        given:
+        BigInteger limit = 0
+        when:
+        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(limit)
+        then:
+        response.initial == limit
+        response.primes == []
+    }
+
+    def 'Calculates primes until -1 with no results'() {
+        given:
+        BigInteger limit = -1
+        when:
+        PrimesResponse response = singleThreadStrategy.calculatePrimesUntilLimit(limit)
+        then:
+        response.initial == limit
+        response.primes == []
     }
 }
